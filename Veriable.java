06d724908df637b67535da22a1437d6c1ad8123f
Ramakant Prajapati <121657019+Pram12345@users.noreply.github.com>
2024-05-24 14:34:49 +0530

    Add files via upload

---
+++ Veriable.java
@@ -0,0 +1,37 @@
+public class Veriable{
+
+public static void main(String args[]){
+
+
+String name = "Ramakant";
+System.out.println(name);
+
+
+int mynumber = 10;
+System.out.println(mynumber);
+
+
+int num;
+num = 20;
+System.out.println(num);
+
+
+
+int mnum = 12;
+mnum = 20;
+System.out.println(mnum);
+
+
+
+int num1 = 4;
+float myFloatNum = 5.55f;
+char myLetter = 'D';
+boolean myBool = true;
+
+
+
+
+}
+
+
+}
