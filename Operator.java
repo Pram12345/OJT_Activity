c68b91e267b0d49eda33353ce302655d460dc067
Ramakant Prajapati <121657019+Pram12345@users.noreply.github.com>
2024-05-24 15:00:37 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,75 @@
+public class Operator{
+
+
+public static void main(String args[]){
+
+int x = 10;
+int y = 20;
+System.out.println(x+y);
+System.out.println(x-y);
+System.out.println(y/x);
+System.out.println(x*y);
+System.out.println(y%x);
+
+
+
+
+
+
+int a = 44;
+++a;  //increment operator
+System.out.println(a);
+
+
+
+
+int b = 30;
+--b;  //decrement operator
+System.out.println(b);
+
+
+
+
+
+//java assignment operator
+
+int c = 10;
+
+int d = 10;
+d += 5;
+
+System.out.println(d);
+
+
+
+
+int x1 = 5;
+int y1 = 3;
+
+System.out.println(x == y); //equl to operator
+
+
+
+
+
+int x2 = 5;
+int y2 = 3;
+System.out.println(x2 != y2); //not equl to operator
+
+
+int x3 = 4;
+int y3 = 3;
+System.out.println(x > y); // greater than operator
+
+
+
+
+
+  
+
+}
+
+
+
+
+}
