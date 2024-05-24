f25096038f39229fea4253edc24f8c9d8fb0b076
Ramakant Prajapati <121657019+Pram12345@users.noreply.github.com>
2024-05-24 15:24:00 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,68 @@
+public class Condition{
+
+public static void main(String args[]){
+
+
+
+//if statements
+
+int x = 50;
+int y = 10;
+if
+ (x > y) {
+  System.out.println("Hello World");
+}
+
+
+
+//if else statement
+
+int time = 20;
+if (time < 18) {
+  System.out.println("Good day.");
+}
+ else 
+
+{
+  System.out.println("Good evening.");
+
+
+}
+
+
+
+//else if statement
+
+
+int time1 = 22;
+if (time1 < 10) {
+  System.out.println("Good morning.");
+} else if (time1 < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+
+//while loop
+
+int i = 0;
+while (i < 5) {
+  System.out.println(i);
+  i++;
+}
+
+
+//for loop 
+
+
+ for (int j = 0; j < 5; j++) {
+      System.out.println(j);
+    }  
+
+
+
+}
+
+
+}
+
